1738a5a176026326e803adc9425a15a7be189c73
Omar Polo <op@omarpolo.com>
2023-09-15 08:16:51 +0000

    change the Scraper interface to return errors
    While being generally better, it also avoids a few nil-deferencing
    crashes in some code paths if failures accurs.

--- goshi/interface.go
+++ goshi/interface.go
@@ -5,11 +5,11 @@ package goshi
 type Scraper interface {
 	// This method fetch all the pages of a chapter and send the
 	// struct Page populated with name and URL of the jpg
-	FetchChapter(chapterURL string, out chan<- Page)
+	FetchChapter(chapterURL string, out chan<- Page) error
 	// This method scrape a manga and return the list of the
 	// available chapters
-	ScrapeChapters(url string) []Chapter
+	ScrapeChapters(url string) ([]Chapter, error)
 	// This method search for a manga and return the list of manga
 	// availables
-	SearchManga(search string) []Manga
+	SearchManga(search string) ([]Manga, error)
 }
--- goshi/scraper/mangaworld.go
+++ goshi/scraper/mangaworld.go
@@ -16,12 +16,16 @@ type MangaWorldScraper struct {
 // BaseURL
 const MangaWorldURL = "https://www.mangaworld.io"
 
-func (m *MangaWorldScraper) SearchManga(input string) []goshi.Manga {
+func (m *MangaWorldScraper) SearchManga(input string) ([]goshi.Manga, error) {
 	urlIta := "/it/it-directory/"
 	search := MangaEdenURL + urlIta + "/?title=" + input
 
 	var mangas []goshi.Manga
-	doc, _ := goquery.NewDocument(search)
+	doc, err := goquery.NewDocument(search)
+	if err != nil {
+		return mangas, err
+	}
+
 	doc.Find(".openManga").Each(func(i int, s *goquery.Selection) {
 		var manga goshi.Manga
 
@@ -30,16 +34,20 @@ func (m *MangaWorldScraper) SearchManga(input string) []goshi.Manga {
 		mangas = append(mangas, manga)
 	})
 
-	return mangas
+	return mangas, nil
 }
 
 // ScrapeChapters take the user search input, scrape with goquery all the
 // the availables chapters and return a slice with all the chapters
-func (m *MangaWorldScraper) ScrapeChapters(input string) []goshi.Chapter {
+func (m *MangaWorldScraper) ScrapeChapters(input string) ([]goshi.Chapter, error) {
 	search := MangaWorldURL + "/manga/1708/" + input
 
 	var chs []goshi.Chapter
-	doc, _ := goquery.NewDocument(search)
+	doc, err := goquery.NewDocument(search)
+	if err != nil {
+		return chs, err
+	}
+
 	doc.Find("a.chap").Each(func(i int, s *goquery.Selection) {
 		var chapter goshi.Chapter
 		chapter.Name, _ = s.Attr("title")
@@ -47,16 +55,20 @@ func (m *MangaWorldScraper) ScrapeChapters(input string) []goshi.Chapter {
 
 		chs = append(chs, chapter)
 	})
-	return chs
+	return chs, nil
 }
 
 // FetchChapter get the chapterURL as input, fetch with goquery all the urls for the
 // jpg images, and send them as a page struct into the channel
 // Then another function will elaborate those structs
-func (m *MangaWorldScraper) FetchChapter(chapterURL string, out chan<- goshi.Page) {
+func (m *MangaWorldScraper) FetchChapter(chapterURL string, out chan<- goshi.Page) error {
 
 	// This orrible hacky workaround, found the last page of the chapter to fetch
-	doc, _ := goquery.NewDocument(chapterURL)
+	doc, err := goquery.NewDocument(chapterURL)
+	if err != nil {
+		return err
+	}
+
 	pag := doc.Find("select.page.custom-select option").Eq(0).First().Text()
 	slashIndex := strings.Index(pag, "/")
 	maxPage, _ := strconv.Atoi(pag[slashIndex+1:])
@@ -75,4 +87,5 @@ func (m *MangaWorldScraper) FetchChapter(chapterURL string, out chan<- goshi.Pag
 		out <- p
 	}
 
+	return nil
 }
